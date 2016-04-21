43f2a479cf9f2a3c23651a097ceaf9b1ac841aae
dalalv <dalalvishal@gmail.com>
2016-04-21 22:12:24 +0800

    slac integration
    slack integration

---
+++ slackNotify.groovy
@@ -0,0 +1,13 @@
+// Orig Ref From :: https://github.com/jenkinsci/pipeline-examples/blob/master/pipeline-examples/slacknotify/slackNotify.groovy
+import groovy.json.JsonOutput
+// Add whichever params you think you'd most want to have
+// replace the slackURL below with the hook url provided by
+// slack when you configure the webhook
+def notifySlack(text, channel) {
+    def slackURL = 'https://hooks.slack.com/services/xxxxxxx/yyyyyyyy/zzzzzzzzzz'
+    def payload = JsonOutput.toJson([text      : text,
+                                     channel   : channel,
+                                     username  : "jenkins",
+                                     icon_emoji: ":jenkins:"])
+    sh "curl -X POST --data-urlencode \'payload=${payload}\' ${slackURL}"
+}
